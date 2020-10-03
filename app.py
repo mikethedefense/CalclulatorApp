6cb9c12e4f705d553d97f52b9a7c7b2529987487
mikethedefense <41405040+mikethedefense@users.noreply.github.com>
2020-10-03 19:27:50 -0400

    Update 1.10
    Added Brackets.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import logging
 
-__version__ = '1.06' 
+__version__ = '1.10' 
 
 logging.basicConfig(level = logging.DEBUG)
 
@@ -17,10 +17,10 @@ class CalculatorApp:
 
         # Title and Geometry
         master.title("Calculator")
-        master.geometry("400x700")
+        master.geometry("400x900")
         master.resizable(False, False)
        
-        # Entry
+       # Entry
         self.answer_entry = Entry(master, textvariable = self.answer)
         self.answer_entry.grid(column = 2, row = 0, pady = 5, ipady = 10, ipadx = 20) 
         self.answer_entry.get() 
@@ -38,10 +38,30 @@ class CalculatorApp:
         self.multiply_button.grid(row = 4, column = 8, ipadx = 25, ipady = 25)
         self.divide_button = Button(master, text = "÷", command = self.divide, font = ("Helvetica", 15, "bold"))
         self.divide_button.grid(row = 5, column = 8, ipadx = 25, ipady = 25)
+        self.equals_button = Button(master, text = "=", command = self.equals, font = ("Helvetica", 15, "bold"))
+        self.equals_button.grid(row = 9, column = 8, ipadx = 25, ipady = 25)
         self.power_button = Button(master, text = "^", command = self.power, font = ("Helvetica", 15, "bold"))
         self.power_button.grid(row = 6, column = 8, ipadx = 25, ipady = 25)
-        self.equals_button = Button(master, text = "=", command = self.equals, font = ("Helvetica", 15, "bold"))
-        self.equals_button.grid(row = 7, column = 8, ipadx = 25, ipady = 25)
+
+        for i in range(10):
+            def _handler(n=i): 
+                self.insert_number(n)
+            btn = Button(master, text=str(i), command=_handler)
+            btn.grid(row=3+i//2, column=i%2, ipadx=25, ipady=25)
+            self.btns.append(btn)  
+        
+        # Decimal Points
+        btn = Button(master, text=str('.'), command=lambda: self.insert_number('.'), font = ("Helvetica", 15, "bold"))
+        btn.grid(row=8, column=0, ipadx=25, ipady=25)
+        self.btns.append(btn)
+
+        # Brackets
+        left_bracket_button = Button(master, text = "(", command = lambda:self.insert_number('('), font = ("Helvetica", 15, "bold"))
+        left_bracket_button.grid(row = 7, column = 8, ipadx = 25, ipady = 25)
+        right_bracket_button = Button(master, text = ")", command = lambda: self.insert_number(')'), font = ("Helvetica", 15, "bold"))
+        right_bracket_button.grid(row = 8, column = 8, ipadx = 25, ipady = 25)
+        self.btns.append(left_bracket_button)
+        self.btns.append(right_bracket_button) 
 
         # Enabled and disabled
         self.plus_button["state"] = DISABLED
@@ -49,92 +69,71 @@ class CalculatorApp:
         self.multiply_button["state"] = DISABLED
         self.divide_button["state"] = DISABLED
         self.equals_button["state"] = DISABLED
+        self.power_button["state"] = DISABLED
         
-        for i in range(10):
-            def _handler(n=i): 
-                self.insert_number(n)
-            btn = Button(master, text=str(i), command=_handler)
-            btn.grid(row=3+i//2, column=i%2, ipadx=25, ipady=25)
-            self.btns.append(btn)
-        # Decimal Points
-        btn = Button(master, text=str('.'), command=lambda: self.insert_number('.'))
-        btn.grid(row=12, column=0, ipadx=25, ipady=25)
-        self.btns.append(btn)
-    
     # Methods
     def add(self):
         """
         Method that will add numbers
         """
+        self.answer_entry.insert(100000, "+")
         self.operation += "".join([str(i) for i in self.number])
         self.number.clear()
-        self.answer_entry.delete(0, END)
         self.operation += '+'
         self.plus_button["state"] = DISABLED
         self.subtract_button["state"] = DISABLED
         self.multiply_button["state"] = DISABLED
         self.divide_button["state"] = DISABLED
         self.equals_button["state"] = DISABLED
+        self.power_button["state"] = DISABLED
         logging.debug(self.operation)
     
     def subtract(self):
         """
         Method that will subtract numbers
         """
+        self.answer_entry.insert(100000, "-")
         self.operation += "".join([str(i) for i in self.number])
         self.number.clear()
-        self.answer_entry.delete(0, END)
         self.operation += '-'
         self.plus_button["state"] = DISABLED
         self.subtract_button["state"] = DISABLED
         self.multiply_button["state"] = DISABLED
         self.divide_button["state"] = DISABLED
         self.equals_button["state"] = DISABLED
+        self.power_button["state"] = DISABLED
         logging.debug(self.operation)
     
     def multiply(self):
         """
         Method that will multiply numbers
         """
+        self.answer_entry.insert(100000, "x")
         self.operation += "".join([str(i) for i in self.number])
         self.number.clear()
-        self.answer_entry.delete(0, END)
         self.operation += '*'
         self.plus_button["state"] = DISABLED
         self.subtract_button["state"] = DISABLED
         self.multiply_button["state"] = DISABLED
         self.divide_button["state"] = DISABLED
         self.equals_button["state"] = NORMAL
+        self.power_button["state"] = DISABLED
         logging.debug(self.operation)
         
     def divide(self):
         """
         Method that will divide numbers
         """
+        self.answer_entry.insert(10000, "÷")
         self.operation += "".join([str(i) for i in self.number])
         self.number.clear()
-        self.answer_entry.delete(0, END)
         self.operation += '/'
         self.plus_button["state"] = DISABLED
         self.subtract_button["state"] = DISABLED
         self.multiply_button["state"] = DISABLED
         self.divide_button["state"] = DISABLED
         self.equals_button["state"] = DISABLED
-        logging.debug(self.operation)
-        
-    def power(self):
-        """
-        Method that will raise numbers to powers
-        """
-        self.operation += "".join([str(i) for i in self.number])
-        self.number.clear()
-        self.answer_entry.delete(0, END)
-        self.operation += '**'
-        self.plus_button["state"] = DISABLED
-        self.subtract_button["state"] = DISABLED
-        self.multiply_button["state"] = DISABLED
-        self.divide_button["state"] = DISABLED
-        self.equals_button["state"] = DISABLED
+        self.power_button["state"] = DISABLED
         logging.debug(self.operation)
     
     def equals(self):
@@ -146,9 +145,10 @@ class CalculatorApp:
         self.multiply_button["state"] = DISABLED
         self.divide_button["state"] = DISABLED
         self.equals_button["state"] = DISABLED
+        self.power_button["state"] = DISABLED
         self.operation += "".join([str(i) for i in self.number])
-        logging.debug(self.operation)
         self.number.clear()
+        logging.debug(self.operation)
         self.answer_entry.delete(0, END)
         try:
             self.answer = eval(self.operation)
@@ -171,15 +171,32 @@ class CalculatorApp:
         """
         Method that will insert a number to the entry
         """
-        self.answer_entry.insert(10000, num) # 10000 digit calculator 
-        self.number.append(num)
+        self.answer_entry.insert(10000, num)
+        self.number.append(num) 
         self.plus_button["state"] = NORMAL
         self.subtract_button["state"] = NORMAL
         self.multiply_button["state"] = NORMAL
         self.divide_button["state"] = NORMAL
         self.equals_button["state"] = NORMAL
+        self.power_button["state"] = NORMAL
         logging.debug(self.number) 
-
+    
+    def power(self):
+        """
+        Method that will raise numbers to powers
+        """
+        self.answer_entry.insert(10000, "^")
+        self.operation += "".join([str(i) for i in self.number])
+        self.number.clear()
+        self.operation += '**'
+        self.plus_button["state"] = DISABLED
+        self.subtract_button["state"] = DISABLED
+        self.multiply_button["state"] = DISABLED
+        self.divide_button["state"] = DISABLED
+        self.equals_button["state"] = DISABLED
+        self.power_button["state"] = DISABLED
+        logging.debug(self.operation)
+       
 # Start Program
 root = Tk()
 logging.debug("Program Started") 
